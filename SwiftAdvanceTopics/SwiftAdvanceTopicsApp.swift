81961848cb1455db4bb60db35d5c5af291614ae6
Shaurya Singh <shaurya2611@gmail.com>
2024-05-21 00:17:36 +0530

    Initial Commit

---
+++ SwiftAdvanceTopics/SwiftAdvanceTopicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAdvanceTopicsApp.swift
+//  SwiftAdvanceTopics
+//
+//  Created by Shaurya Singh on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAdvanceTopicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
