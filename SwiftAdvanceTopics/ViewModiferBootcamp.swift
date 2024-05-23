1cc6e733acd770978d47cfc3fbf853ed2cff285b
Shaurya Singh <shaurya2611@gmail.com>
2024-05-23 19:59:36 +0530

    ViewModifier use case

---
+++ SwiftAdvanceTopics/ViewModiferBootcamp.swift
@@ -0,0 +1,45 @@
+//
+//  ViewModifer.swift
+//  SwiftAdvanceTopics
+//
+//  Created by Shaurya Singh on 21/05/24.
+//
+
+import SwiftUI
+
+// View modifier helps in code reusability
+// modifier --> applied on existing virew and return an new view
+struct DefaultButtonViewModifer: ViewModifier{
+    
+    let backgroundColor: Color
+    let foregroundColor: Color
+
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .frame(maxWidth: .infinity, minHeight: 40)
+            .background(backgroundColor)
+            .foregroundColor(foregroundColor)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            .padding(20)
+    }
+}
+
+extension View{
+    func withDefaultButtonFormatting(backgroundColor: Color, foregroundColor: Color) -> some View {
+        modifier(DefaultButtonViewModifer(backgroundColor: backgroundColor, foregroundColor: foregroundColor))
+    }
+}
+
+struct ViewModiferBootcamp: View {
+    var body: some View {
+        VStack{
+            Text("LogIn").withDefaultButtonFormatting(backgroundColor: .blue, foregroundColor: .white)
+        }
+    }
+}
+
+#Preview {
+    ViewModiferBootcamp()
+}
